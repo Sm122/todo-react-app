bc891b82949cd6e3b327c79482e5ea3ae1334ab1
Sourabh Maheshwari <38175895+Sm122@users.noreply.github.com>
2020-12-09 18:32:22 +0530

    Addtodo complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 
 import React, { Component } from 'react';
 import './App.css';
-import Todos from './components/Todos'
-import Header from './components/layout/Header'
-import Addtodo from './components/Addtodo'
+import Todos from './components/Todos';
+import Header from './components/layout/Header';
+import Addtodo from './components/Addtodo';
+import {v4 as uuid} from 'uuid';  
 
  class App extends Component {
   state = {
     todos: [
       {
-        id : 1,
+        id : uuid(),
         title : 'Take out the Trash',
         completed : false
       },
       {
-        id : 2,
+        id : uuid(),
         title : 'Doing Coding Work',
         completed : true
       },
       {
-        id : 3,
+        id : uuid(),
         title : 'Meeting with Manager',
         completed : false
       }
@@ -43,13 +44,25 @@ import Addtodo from './components/Addtodo'
      )] }); 
    }
 
+   // add Todo
+   addTodo = (title) =>{
+     const newTodo = {
+       id : uuid(),
+       title,
+       completed : false,
+     }
+      this.setState({todos : [...this.state.todos, newTodo]
+
+      })
+   }
+
   render() {
     console.log(this.state.todos);
   return (
     <div className="App">
       <div className="container">
       <Header/>
-      <Addtodo/>
+      <Addtodo addTodo = {this.addTodo}/>
       <Todos todos = {this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/> 
       </div>
     
